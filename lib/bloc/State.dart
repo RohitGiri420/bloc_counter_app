447ee1603971fdad2f3ad5edd786ff340ebbf14e
Rohit Giri <giri420rohit@gmail.com>
2024-03-20 17:57:00 +0530

    reset whole project

--- lib/bloc/State.dart
+++
@@ -1,8 +0,0 @@
-class CounterState{}
-
-class InitialState extends CounterState{}
-
-class UpdatedState extends CounterState{
-  int count;
-  UpdatedState(this.count);
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:bloc_counter_app/bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'Screen/CounterScreen.dart';
 
 void main(){
   runApp(MyApp());
@@ -12,11 +9,8 @@ void main(){
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => CounterBloc(),
-      child: MaterialApp(
-        home: CounterScreen(),
-      ),
+    return MaterialApp(
+      home: CounterScreen(),
     );
   }
 
